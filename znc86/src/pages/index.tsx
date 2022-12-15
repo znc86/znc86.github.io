8565d9d7735822955c4a808936c1c8c91969fcdf
0xADADA <0xadada.pub@0xadada.pub>
2022-12-15 12:09:45 -0500

    roughed in page layout and github action

--- znc86/src/pages/index.tsx
+++ znc86/src/pages/index.tsx
@@ -1,6 +1,7 @@
 import { type NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
+import Page from "../components/page";
 import styles from "./index.module.css";
 
 const Home: NextPage = () => {
@@ -10,15 +11,6 @@ const Home: NextPage = () => {
         <title>
           ZNC86.club - motorsports support team for the Z N/C 8/6 chassis
         </title>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link
-          rel="icon"
-          type="image/png"
-          href="https://znc86.club/favicon.png"
-          sizes="64x64"
-        />
-        <link rel="apple-touch-icon" href="https://znc86.club/favicon.png" />
 
         <meta property="og:site_name" content="ZNC86" />
         <meta
@@ -34,93 +26,96 @@ const Home: NextPage = () => {
         <meta property="og:image:width" content="1260" />
         <meta property="og:image:height" content="1260" />
       </Head>
-      <div className={styles.page}>
-        <h1>ZNC86.club</h1>
+      <Page>
+        <div className={styles.page}>
+          <h1>ZNC86.club</h1>
 
-        <h2>motorsports support team for the Z N/C 8/6 chassis</h2>
+          <h2>motorsports support team for the Z N/C 8/6 chassis</h2>
 
-        <div className={styles.layout}>
-          <div className={styles.one}>
-            <p>
-              ZNC86 is <em>not an online community</em> but a
-              <strong>group of friends </strong>
-              sharing resources, attending events, learning, solving problems,
-              having fun, and supporting each other to enjoy the pleasures of
-              motoring sport in the Z N/C 8/6 chassis.
-            </p>
+          <div className={styles.layout}>
+            <div className={styles.one}>
+              <p>
+                ZNC86 is <em>not an online community</em> but a
+                <strong>group of friends </strong>
+                sharing resources, attending events, learning, solving problems,
+                having fun, and supporting each other to enjoy the pleasures of
+                motoring sport in the Z N/C 8/6 chassis.
+              </p>
 
-            <p>
-              We drive the
-              <ruby>
+              <p>
+                We drive the
                 <ruby>
-                  Z<rp>(</rp>
-                  <rt>ZN6</rt>
-                  <rp>)</rp> N/C<rp>(</rp>
-                  <rt>ZN8</rt>
-                  <rp>)</rp> 8/6<rp>(</rp>
-                  <rt>ZC8 ZC6</rt>
-                  <rp>)</rp>
+                  <ruby>
+                    Z<rp>(</rp>
+                    <rt>ZN6</rt>
+                    <rp>)</rp> N/C<rp>(</rp>
+                    <rt>ZN8</rt>
+                    <rp>)</rp> 8/6<rp>(</rp>
+                    <rt>ZC8 ZC6</rt>
+                    <rp>)</rp>
+                  </ruby>
+                  <rp> </rp>
                 </ruby>
-                <rp> </rp>
-              </ruby>
-              chassis of many names: Scion FR-S, Toyota 86, Toyota GR86, Subaru
-              BRZ. You can find us at Club Loose, COM SCC time attack, Lock City
-              Drift, North East Drift Coalition, MassTuning events, and more.
-            </p>
+                chassis of many names: Scion FR-S, Toyota 86, Toyota GR86,
+                Subaru BRZ. You can find us at Club Loose, COM SCC time attack,
+                Lock City Drift, North East Drift Coalition, MassTuning events,
+                and more.
+              </p>
 
-            <p lang="es">
-              ZNC86 no es una comunidad online, sino un grupo de amigos que
-              comparten recursos, asisten a eventos, aprenden, resuelven
-              problemas, se divierten y se apoyan mutuamente para disfrutar de
-              los placeres del deporte del motor en el chasis Z N/C 8/6.
-            </p>
+              <p lang="es">
+                ZNC86 no es una comunidad online, sino un grupo de amigos que
+                comparten recursos, asisten a eventos, aprenden, resuelven
+                problemas, se divierten y se apoyan mutuamente para disfrutar de
+                los placeres del deporte del motor en el chasis Z N/C 8/6.
+              </p>
 
-            <p lang="ja">
-              ZNC86はオンラインコミュニティではなく、リソースを共有し、イベントに参加し、
-              学び、問題を解決し、楽しみ、そしてZ N/C
-              8/6シャーシでのモータースポーツ
-              の楽しみをサポートし合う仲間たちのグループです。
-            </p>
-            <Image
-              src="/icon.png"
-              alt="pixel art of a car"
-              width="320"
-              height="320"
-            />
+              <p lang="ja">
+                ZNC86はオンラインコミュニティではなく、リソースを共有し、イベントに参加し、
+                学び、問題を解決し、楽しみ、そしてZ N/C
+                8/6シャーシでのモータースポーツ
+                の楽しみをサポートし合う仲間たちのグループです。
+              </p>
+              <Image
+                src="/icon.png"
+                alt="pixel art of a car"
+                width="320"
+                height="320"
+              />
+            </div>
+            <ul className={`${styles.two} ${styles.links}`}>
+              <li>
+                <a href="conduct.html" title="Code of Conduct">
+                  ⚖️ Code of Conduct
+                </a>
+                <br />
+                Friendly, inclusive, and simple
+              </li>
+              <li>
+                <a
+                  className={styles.discord}
+                  href="https://forms.gle/4wmUwEzHdQjkgezo7"
+                  title="Join the Discord chat"
+                >
+                  Join the Club
+                </a>
+                <br />
+                Fill out the form and start chatting on Discord
+              </li>
+              <li>
+                <a
+                  className={styles.instagram}
+                  href="https://instagram.com/znc86.club"
+                  rel="external"
+                >
+                  Instagram
+                </a>
+                <br />
+                Follow us @znc86.club
+              </li>
+            </ul>
           </div>
-          <ul className={`${styles.two} ${styles.links}`}>
-            <li>
-              <a href="conduct.html" title="Code of Conduct">
-                ⚖️ Code of Conduct
-              </a>
-              <br />
-              Friendly, inclusive, and simple
-            </li>
-            <li>
-              <a
-                className={styles.discord}
-                href="https://forms.gle/4wmUwEzHdQjkgezo7"
-                title="Join the Discord chat"
-              >
-                Join the Club
-              </a>
-              <br />
-              Fill out the form and start chatting on Discord
-            </li>
-            <li>
-              <a
-                className={styles.instagram}
-                href="https://instagram.com/znc86.club"
-                rel="external"
-              >
-                Instagram
-              </a>
-              <br />
-              Follow us @znc86.club
-            </li>
-          </ul>
         </div>
-      </div>
+      </Page>
     </>
   );
 };

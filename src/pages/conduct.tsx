824d3d2d3f207de50bdf658e73eee42ce638c5ad
0xADADA <0xadada.pub@0xadada.pub>
2022-12-16 09:55:14 -0500

    translated code of conduct

--- src/pages/conduct.tsx
+++ src/pages/conduct.tsx
@@ -21,66 +21,38 @@ const Home: NextPage = () => {
       <Head>
         <title>{t("pages.conduct.title")}</title>
 
-        <meta property="og:title" content={t("pages.conduct.title")} />
-        <meta name="description" content="ZNC86 Code of Conduct" />
-        <meta property="og:url" content="https://znc86.club/conduct" />
+        <meta property="og:title" content={t("pages.conduct.title") || ""} />
+        <meta
+          name="description"
+          content={t("pages.conduct.display title") || ""}
+        />
+        <meta property="og:url" content="/conduct" />
       </Head>
       <Page>
-        <h1>ZNC86 Code of Conduct</h1>
-        <p>Last updated 2022/11/21</p>
+        <h1>{t("pages.conduct.display title")}</h1>
+        <p>{t("pages.conduct.last updated")}</p>
 
         <p>
-          <strong>TL;DR</strong>
-          Make friends not enemies. Strive for kindness, be polite, assume
-          others are acting in good faith, be gracious when others are not. Do
-          not tolerate assholes, negativity, report harassment and hate. Act
-          like youʼll meet these people face-to-face tomorrow.
+          <strong>TL;DR</strong> {t("pages.conduct.make friends")}
         </p>
 
         <p>
-          <strong>Diversity: Everyone is Welcome</strong>
-          We are inclusive and committed to being a community that everyone
-          feels good about joining. Although we may not be able to satisfy
-          everyone, we will always work to treat everyone well.
+          <strong>{t("pages.conduct.diversity")}</strong>{" "}
+          {t("pages.conduct.inclusive")}
         </p>
 
-        <p>This Code of Conduct applies IRL and online.</p>
+        <p>{t("pages.conduct.applies")}</p>
 
-        <h2>Code of Conduct</h2>
+        <h2>{t("pages.conduct.subtitle")}</h2>
 
         <ol>
-          <li>
-            No discrimination based on gender, sexual orientation, disability,
-            physical appearance, race, age, ethnicity, language, religion (or
-            lack thereof), national origin, or otherwise.
-          </li>
-          <li>
-            No stalking or harassment, this includes offensive verbal comments
-            related to (1), no posting sexual images in public spaces,
-            deliberate intimidation, following, harassing photography or
-            recording, cyberbullying, disruption of discussions or events,
-            shitposting, no inappropriate physical contact, and no unwelcome
-            attention.
-          </li>
-          <li>
-            No incitement of violence or promotion of violent ideologies, no
-            xenophobia, no transphobia, no violent nationalism, no antisemitism.
-          </li>
-          <li>
-            No ad-hominem attacks, name calling, or speech about a person or
-            persons that is negative or harmful to them if the intent is to
-            damage the person either emotionally or financially, or lowers them
-            in the estimation of others.
-          </li>
-          <li>
-            Sexual language and imagery is not appropriate. No sexual depictions
-            of people under 18 years of age.
-          </li>
-          <li>
-            Do not share intentionally false or misleading information. No
-            conspiracy theories.
-          </li>
-          <li>No commercial accounts, advertising, or excessive promotion.</li>
+          <li>{t("pages.conduct.one")}</li>
+          <li>{t("pages.conduct.two")}</li>
+          <li>{t("pages.conduct.three")}</li>
+          <li>{t("pages.conduct.four")}</li>
+          <li>{t("pages.conduct.five")}</li>
+          <li>{t("pages.conduct.six")}</li>
+          <li>{t("pages.conduct.seven")}</li>
         </ol>
       </Page>
     </>

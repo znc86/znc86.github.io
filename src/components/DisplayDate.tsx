d30b226cc10b253b030ec70af595a1e7b61e5ffe
0xADADA <0xadada.pub@0xadada.pub>
2022-12-16 22:49:40 -0500

    fixed time

--- src/components/DisplayDate.tsx
+++ src/components/DisplayDate.tsx
@@ -2,7 +2,7 @@ import { format } from "date-fns";
 
 const TOKEN = "EEEE LLLL d, yyyy";
 
-export default function DateFormatter({ datetime }) {
+export default function DateFormatter({ datetime }: { datetime: string }) {
   const date = new Date(datetime);
   const displayDate = format(date, TOKEN);
   const dateTime = date.toISOString();

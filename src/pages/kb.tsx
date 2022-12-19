84917b9fd5415fef16968ed4cbd0be9a4fe4396c
0xADADA <0xadada.pub@0xadada.pub>
2022-12-19 11:18:53 -0500

    added og tags to kb

--- src/pages/kb.tsx
+++ src/pages/kb.tsx
@@ -4,8 +4,8 @@ import { useTranslation } from "next-i18next";
 import Page from "../components/page";
 import { PostCard } from "../components/PostCard";
 import { loadBlogPosts } from "../lib/loader";
-import type { NextPage } from "next";
 import styles from "./kb.module.css";
+import type { PostData } from "../lib/loader";
 
 type HomeProps = {
   locale: string;
@@ -21,18 +21,29 @@ export async function getStaticProps({ locale }: HomeProps) {
   };
 }
 
-const Kb: NextPage = (props: any) => {
+interface KbProps {
+  posts: PostData[];
+}
+export default function Kb({ posts }: KbProps) {
   const { t } = useTranslation("common");
 
   return (
     <>
       <Head>
         <title>{t("pages.kb.page title")}</title>
+        <meta property="og:title" content={t("pages.kb.page title") || ""} />
+        <meta name="description" content={t("pages.kb.kb subtitle") || ""} />
+        <meta name="og:description" content={t("pages.kb.kb subtitle") || ""} />
+        <meta property="og:url" content="https://znc86.club/kb" />
+        <meta property="og:image" content="https://znc86.club/icon.png" />
+        <meta property="og:image:width" content="1260" />
+        <meta property="og:image:height" content="1260" />
       </Head>
       <Page>
         <h1>{t("pages.kb.title")}</h1>
+        <p>{t("pages.kb.kb subtitle")}</p>
         <ol className={styles.list}>
-          {props.posts.map((post: any, j: any) => {
+          {posts.map((post: PostData, j: number) => {
             return (
               <li key={j}>
                 <PostCard post={post} />
@@ -43,6 +54,4 @@ const Kb: NextPage = (props: any) => {
       </Page>
     </>
   );
-};
-
-export default Kb;
+}

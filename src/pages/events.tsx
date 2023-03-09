a3834778096d947c75615807fbac3345e598d460
0xADADA <0xadada.pub@0xadada.pub>
2023-03-08 19:08:39 -0500

    Update events.tsx

--- src/pages/events.tsx
+++ src/pages/events.tsx
@@ -22,10 +22,32 @@ export default function Kb() {
   const { t } = useTranslation("common");
 
   const calendars = [
-    { calendarId: "dpshlst16bu2i322nie1rs4u9tru2ksi@import.calendar.google.com" }, /* motorsports */
     {
       calendarId: "jo2v8hedovnfbe9n659libt3j07bn4ec@import.calendar.google.com", /* lock city drift */
+    },
+    {
+      calendarId: "4305ivosuadn1nn5mv7f2gng0vcijk5t@import.calendar.google.com" }, /* COM SCC */
       color: "#ff007f",
+    }, 
+    {
+      calendarId: "rf2eba9s6ugbpsnp1u2kih5dtdrujmjb@import.calendar.google.com" }, /* MassTuning */
+      color: "#80FF00",
+    },
+    {
+      calendarId: "cjsf77dn3tfc7stmms4d8mbvhk6gt9au@import.calendar.google.com" }, /* Club Loose */
+      color: "#8000FF",
+    },
+    {
+      calendarId: "d7bjifjscgbtg7qgcpv9f402jknkl56a@import.calendar.google.com" }, /* Northeast Track Club */
+      color: "#0000FF",
+    }, 
+    {
+      calendarId: "mskdmb3b2tul7o8b72cg8dgorevgagsl@import.calendar.google.com" }, /* GRIDLIFE */
+      color: "#00FFFF",
+    },
+    {
+      calendarId: "vbteo7ik5psq8sk3jlf7ddqkoelmsh5h@import.calendar.google.com" }, /* Final Bout, etc */
+      color: "#00FF80",
     },
   ];
 
